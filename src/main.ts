bf6de91a77cd30dd229d7ce1cce99cf9ddbb2feb
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-09-09 08:28:52 +0100

    selection sort

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { binarySearch } from "./arrays/searching/binarySearch";
 import { linearSearch } from "./arrays/searching/linearSearch";
 import { bubbleSort } from "./arrays/sorting/bubbleSort";
+import { selectionSort } from "./arrays/sorting/selectionSort";
 
 // console.log(linearSearch([21, 89, 63, 26, 47, 32], 26));
 // console.log(binarySearch([11, 25, 30, 36, 23, 41, 60], 41));
 // console.log(linearSearch(["a", "e", "i", "o", "u"], "a"));
 
-console.log(bubbleSort([45, 21, 13, 28, 9, 11]));
+// console.log(bubbleSort([45, 21, 13, 28, 9, 11]));
+console.log(selectionSort([45, 21, 13, 28, 9, 11]));
