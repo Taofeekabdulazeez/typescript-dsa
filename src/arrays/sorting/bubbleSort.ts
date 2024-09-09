e1809ea3c9d988fc6cc92189578ebf9aa730bc99
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-09-09 07:13:52 +0100

    bubble sort

--- src/arrays/sorting/bubbleSort.ts
+++ src/arrays/sorting/bubbleSort.ts
@@ -1,1 +1,15 @@
-function bubbleSort(arr: number[]) {}
+function bubbleSort<T>(arr: T[]): T[] {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+export { bubbleSort };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { binarySearch } from "./arrays/searching/binarySearch";
 import { linearSearch } from "./arrays/searching/linearSearch";
+import { bubbleSort } from "./arrays/sorting/bubbleSort";
 
-console.log(linearSearch([21, 89, 63, 26, 47, 32], 26));
-console.log(binarySearch([11, 25, 30, 36, 23, 41, 60], 41));
-console.log(linearSearch(["a", "e", "i", "o", "u"], "a"));
+// console.log(linearSearch([21, 89, 63, 26, 47, 32], 26));
+// console.log(binarySearch([11, 25, 30, 36, 23, 41, 60], 41));
+// console.log(linearSearch(["a", "e", "i", "o", "u"], "a"));
+
+console.log(bubbleSort([45, 21, 13, 28, 9, 11]));
