0799fbfffa50f4f45ae5c58a81f815f2105b5cd9
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-11-16 15:14:08 +0100

    implemented single-linked-list

---
+++ src/LinkedList/SingleLinkedList.ts
@@ -0,0 +1,48 @@
+class ListNode {
+  value: any;
+  next: ListNode | null;
+
+  constructor(value: any) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+export class SingleLinkedList {
+  head: ListNode | null;
+  tail: ListNode | null;
+  length: number;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value: any) {
+    const newListNode = new ListNode(value);
+    if (!this.head) {
+      this.head = newListNode;
+      this.tail = newListNode;
+    } else {
+      this.tail!.next = newListNode;
+      this.tail = newListNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.length) return undefined;
+    let current = this.head;
+    // while (current) {}
+  }
+
+  traverse() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,19 @@ import { binarySearch } from "./arrays/searching/binarySearch";
 import { bubbleSort } from "./arrays/sorting/bubbleSort";
 import { insertionSort } from "./arrays/sorting/insertionSort";
 import { selectionSort } from "./arrays/sorting/selectionSort";
+import { SingleLinkedList } from "./LinkedList/SingleLinkedList";
 
-console.log(binarySearch([76, 65, 30, 80, 12, 36], 80));
-console.log("Bubble sort ==>", bubbleSort([76, 65, 30, 80, 12, 36]));
-console.log("Selection sort ==>", selectionSort([76, 65, 30, 80, 12, 36]));
-console.log("Insertion sort ==>", insertionSort([76, 65, 30, 80, 12, 36]));
+// console.log(binarySearch([76, 65, 30, 80, 12, 36], 80));
+// console.log("Bubble sort ==>", bubbleSort([76, 65, 30, 80, 12, 36]));
+// console.log("Selection sort ==>", selectionSort([76, 65, 30, 80, 12, 36]));
+// console.log("Insertion sort ==>", insertionSort([76, 65, 30, 80, 12, 36]));
+
+const list = new SingleLinkedList();
+
+list.push(10);
+list.push(20);
+list.push(30);
+
+console.log(list);
+
+list.traverse();
